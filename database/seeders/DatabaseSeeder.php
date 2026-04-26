729a857ce60d4961bb18e091558dad8ea1f8e434
ncoode13 <ncode955@gmail.com>
2026-04-26 13:49:01 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,5 +32,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AnnouncementCategorySeeder::class);
         // $this->call(AnnouncementSeeder::class);
         $this->call(EmployeeExcelSeeder::class);
+
     }
 }
